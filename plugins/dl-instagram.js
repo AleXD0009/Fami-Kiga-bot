b3a6aaa7f80b2ab384d065f9ed5f47b4cd2b4bf1
JTxs <145930757+AleXD0009@users.noreply.github.com>
2026-07-17 14:17:23 -0300

    Fami Kiga

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,19 +1,42 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '*Ingresa el enlace del vídeo de Instagram junto al comando*', m)
+let FM = async (m, { conn, args }) => {
+let usrname = conn.getName ? conn.getName(m.sender) : m.pushName || 'Usuario'
+
+if (!args[0]) return conn.reply(m.chat, `❀ 𝖧𝗈𝗅𝖺 *${usrname}*, 𝗂𝗇𝗀𝗋𝖾𝗌𝖺 𝗎𝗇 𝖾𝗇𝗅𝖺𝖼𝖾 𝖽𝖾 *𝖨𝗇𝗌𝗍𝖺𝗀𝗋𝖺𝗆* 𝗏á𝗅𝗂𝖽𝗈.`, m, rcanal)
 
 try {
-m.react('🕑')
-let { dl_url } = await Starlights.igdl(args[0])
-await conn.sendFile(m.chat, dl_url, 'igdl.mp4', null, m, null, rcanal)
-m.react('✅')
-} catch {
-m.react('❌')
+let res = await fetch(`${global.apist}/api/download/instagram?url=${encodeURIComponent(args[0])}`)
+let json = await res.json()
+if (!json.status || !json.data || !json.data.result || json.data.result.length === 0) { await m.react('❌') }
+let { result } = json.data
+if (result.length === 1) {
+let item = result[0]
+let buffer = await fetch(item.dl_url).then(r => r.buffer())
+if (item.type.toLowerCase() === 'video') {
+await conn.sendMessage(m.chat, { video: buffer, mimetype: 'video/mp4', fileName: 'igdl.mp4', caption: null }, { quoted: m })
+} else {
+await conn.sendMessage(m.chat, { image: buffer, caption: null }, { quoted: m })
+}} else {
+let album = []
+for (let i = 0; i < result.length; i++) {
+let item = result[i]
+let buffer = await fetch(item.dl_url).then(r => r.buffer())
+if (item.type.toLowerCase() === 'video') {
+album.push({ video: buffer, caption: i === 0 ? `` : '', })
+} else {
+album.push({ image: buffer, caption: i === 0 ? `` : '', })
+}}
+await conn.sendMessage(m.chat, { album }, { quoted: m })
+}
+
+} catch (error) {
+console.log(error)
+await m.react('❌')
 }}
 
-handler.help = ['instagram *<link>*']
-handler.tags = ['dl']
-handler.command = ['instagramdl', 'instagram', 'igdl', 'ig']
+FM.help = ['instagram ( Link )']
+FM.tags = ['dl']
+FM.command = ['instagramdl', 'instagram', 'igdl', 'ig']
 
-export default handler
+export default FM
