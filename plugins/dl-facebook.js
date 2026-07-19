4465376aa0567f07a6dfbef95fd8f6d0ca05573d
JTxs <145930757+AleXD0009@users.noreply.github.com>
2026-07-19 12:44:56 -0300

    Fami Kiga

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -7,15 +7,7 @@ if (!args[0]) return conn.reply(m.chat, `❀ 𝖧𝗈𝗅𝖺 *${usrname}*, 𝗂
 try {
 let res = await fetch(`${global.apist}/api/download/facebook?url=${encodeURIComponent(args[0])}`)
 let json = await res.json()
-if (!json.status || !json.data) {
-await m.react('❌')
-}
 let { title, hd, sd } = json.data
-let videoUrl = hd || sd
-
-if (!videoUrl) {
-await m.react('❌')
-}
 let dl_url = await fetch(hd || sd).then(r => r.buffer())
 await conn.sendMessage(m.chat, { video: dl_url, mimetype: 'video/mp4', fileName: 'video.mp4', caption: null }, { quoted: m })
 } catch (error) {
